eb6463d5a94db15a78d4fc9feb17bcc3d90e8665
Francesco Santoro <francesco.santoro@6wind.com>
2016-01-29 13:17:39 +0100

    6wind-virtual-accelerator-plugin: initial commit
    This initial commit creates and populates the 6WIND virtual
    accelerator fuel plugin.
    It is based on the Fuel Plugins 3.0 skeleton and it uses puppet
    modules for deployment tasks.
    
    Signed-off-by: Francesco Santoro <francesco.santoro@6wind.com>
    Acked-by: Samuel Gauthier <samuel.gauthier@6wind.com>
    Acked-by: Vincent Jardin <vincent.jardin@6wind.com>
    
    Change-Id: Iea2ab728669d89a209bfe03d13a66a48f5c5b510

---
+++ deployment_scripts/puppet/modules/virtual_accelerator/files/config_va.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+fpconf="/usr/local/etc/fast-path.env"
+
+function set_fp_opt() {
+	if grep -q '^#\?: \${'"$1"':=.*}' "$3"; then
+		sed -i -- 's@^#\?: \${'"$1"':=.*@: ${'"$1"':='"$2"'}@' "$3";
+	else
+		echo >> $3;
+		echo '# Config added by VA fuel plugin' >> $3;
+		echo ': ${'"$1"':='"$2"'}' >> $3;
+	fi
+}
+
+set_fp_opt "$1" "$2" $fpconf
