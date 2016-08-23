52178ec70beb111b6eadd2e8d8e9feb646784b60
Francesco Santoro <francesco.santoro@6wind.com>
2016-08-23 15:53:18 +0200

    6wind-virtual-accelerator-plugin: add support for multiple product version
    This patch adds a field to define which version of 6WIND Virtual
    Accelerator will be installed.
    Moreover the default value now points to the last stable version thus we
    need to make sure to disable ipset ONLY when using the Virtual
    Accelerator 1.3.
    
    Signed-off-by: Francesco Santoro <francesco.santoro@6wind.com>
    
    Change-Id: Ia314535b520b4ac9e916a497366f075095d57335

--- deployment_scripts/puppet/modules/virtual_accelerator/manifests/neutron_conf.pp
+++ deployment_scripts/puppet/modules/virtual_accelerator/manifests/neutron_conf.pp
@@ -6,18 +6,18 @@ class virtual_accelerator::neutron_conf inherits virtual_accelerator {
   $advanced_params = $virtual_accelerator::advanced_params
 
   $disable_ipset = $virtual_accelerator::disable_ipset
+  $va_version = $virtual_accelerator::va_version
 
-    if $disable_ipset == true {
+  if $disable_ipset == true or $va_version == '1.3' {
+      $OVS_CONF_FILE = "/etc/neutron/plugins/ml2/ml2_conf.ini"
 
-	  $OVS_CONF_FILE = "/etc/neutron/plugins/ml2/ml2_conf.ini"
-
-	  package { 'crudini':
-	      ensure => 'latest',
-	  } ->
-	  exec { 'disable_ipset':
-	     command => "crudini --set ${OVS_CONF_FILE} securitygroup enable_ipset False",
-	     notify => Service['openvswitch-switch'],
-	  }
+      package { 'crudini':
+         ensure => 'latest',
+      } ->
+      exec { 'disable_ipset':
+         command => "crudini --set ${OVS_CONF_FILE} securitygroup enable_ipset False",
+         notify => Service['openvswitch-switch'],
+      }
 
       service { 'openvswitch-switch':
          ensure => 'running',
