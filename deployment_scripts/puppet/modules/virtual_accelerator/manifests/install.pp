c8b0e430a46d86aba67c155919a3c8cc5fac6c0f
Francesco Santoro <francesco.santoro@6wind.com>
2016-08-29 12:57:44 +0200

    6wind-virtual-accelerator-plugin: support for Mellanox NICs
    This commit enables support for 6WIND Virtual Accelerator addon for
    Mellanox NICs support.
    
    Signed-off-by: Francesco Santoro <francesco.santoro@6wind.com>
    
    Change-Id: I57d9c3ac29fffda0d77058dd637df03a15ec5ada

--- deployment_scripts/puppet/modules/virtual_accelerator/manifests/install.pp
+++ deployment_scripts/puppet/modules/virtual_accelerator/manifests/install.pp
@@ -3,12 +3,21 @@
 
 class virtual_accelerator::install inherits virtual_accelerator {
 
+  $mellanox_support = $virtual_accelerator::mellanox_support
+
   exec { 'update_repos':
       command => '/usr/bin/apt-get -y update',
   } ->
   package { 'virtual-accelerator':
     ensure          => 'installed',
     install_options => ['--allow-unauthenticated'],
+  }
+
+  if $mellanox_support == true {
+    package { 'virtual-accelerator-addon-mellanox':
+      ensure          => 'installed',
+      install_options => ['--allow-unauthenticated'],
+    }
   }
 
   file { '/usr/local/bin/config_va.sh':
